385ebf94625ad84a8d4978f6ec43811a1f4c22eb
Jeremy <jereamon@gmail.com>
2019-07-26 11:28:05 -0700

    Activity completed preparing to submit.

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ import requests
 
 if __name__ == "__main__":
     username = sys.argv[1]
+    base_url = "https://api.github.com"
 
     # TODO:
     #
     # 1. Retrieve a list of "events" associated with the given user name
     # 2. Print out the time stamp associated with the first event in that list.
+    response = requests.get(base_url + '/users/' + username + '/events')
+    first_event_timestampt = json.loads(response.content)[0]['created_at']
 
-    print("COMPLETE THE TODOs")
+    # print("COMPLETE THE TODOs")
+    print(first_event_timestampt)
     
 
 
